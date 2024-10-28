10d9f45fa7393981e70c0f005fb30d31c1002ba6
Asfiya <asfiyahussain46@gmail.com>
2024-10-28 13:37:20 +0500

    Initial commit

---
+++ Assignments/Chapter1/app.js
@@ -0,0 +1,32 @@
+// chapter no 1 // alert
+
+//1. Write a script to greet your website visitor using JS alert
+//box.
+// alert("Hello World");
+
+//2. Write a script to display following message on your web
+//page:
+// alert("Error! please enter a valid password");
+
+//3. Write a script to display following message on your web
+//page: (Hint : Use line break)
+// alert("Well Come to JS Land \n Happy Coding!");
+
+
+//4. Write a script to display following messages in sequence:
+// alert("wellcome to JS land ");
+// alert("happy coding!\n prevent this page from creating additional dialogs");
+
+
+//5. Generate the following message through browser’s
+//developer console:
+// alert("hello...i can run js through my browser's console");
+
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+// var display= function() {
+//     alert("HELLO VISITOR");}
+//    display();
+
+
+
---
+++ Assignments/Chapter2/app.js
@@ -0,0 +1,64 @@
+// / 1. Declare a variable called username.
+var username = "Asfiya Hussain";
+
+// 2. Declare a variable called myName & assign to it a string 
+var myName = "Asfiya";
+alert(myName);
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var a = "Hello World";
+alert(a);
+
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+let studentData = "Name: Asfiya \n FatherName : Hussain \n Coruse: WAM \n RollNo:1234" ;
+alert(studentData);
+
+
+// Write a script to display the following alert using one JS
+// variable:
+let pizza = "PIZZA \n PIZZ \n PI \n P";
+alert(pizza);
+
+
+//6. Declare a variable called email and assign to it a string that
+//represents your Email Address(e.g. example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: use
+//string concatenation)
+
+let email = "asfiyahussain46@gmail.com";
+alert("My email is: "  + email);
+
+
+//7. Declare a variable called book & give it the value “A
+//smarter way to learn JavaScript”. Display the following
+//message in an alert box:
+
+let book = " I am trying to learn from the book Smarter Way to Learn Javascript";
+alert(book);
+
+// 8. Write a script to display this in browser through JS
+
+ let a ="Yahhh! I can write Html through the javascript"
+
+
+
+//  9. Store following string in a variable and show in alert and
+//  browser through JS
+ let string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+ alert(string);
+
+
+
+
+
+
+
+
